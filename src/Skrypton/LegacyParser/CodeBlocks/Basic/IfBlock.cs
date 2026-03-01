60c20627aef34c869f9e931549f06b5d6bf170de
Luben Naumov <luben.naumov@helpline.pmcs.de>
2026-03-01 12:43:46 +0100

    CA2235

--- src/Skrypton/LegacyParser/CodeBlocks/Basic/IfBlock.cs
+++ src/Skrypton/LegacyParser/CodeBlocks/Basic/IfBlock.cs
@@ -7,9 +7,8 @@ using Skrypton.LegacyParser.CodeBlocks.SourceRendering;
 
 namespace Skrypton.LegacyParser.CodeBlocks.Basic
 {
-    [Serializable]
     [DataContract(Namespace = "http://vbs")]
-    public class IfBlock : IHaveNestedContent
+    public sealed class IfBlock : IHaveNestedContent
     {
         // =======================================================================================
         // CLASS INITIALISATION
