3113ae1090e1a61dc7b34ef3ec3882b5c4242339
Luben Naumov <luben.naumov@helpline.pmcs.de>
2026-03-02 12:23:00 +0100

    CA1040

--- src/Skrypton/CSharpWriter/CodeTranslation/IOutermostScope.cs
+++ src/Skrypton/CSharpWriter/CodeTranslation/IOutermostScope.cs
@@ -1,6 +1,8 @@
 ﻿namespace Skrypton.CSharpWriter.CodeTranslation
 {
+#pragma warning disable CA1040 // Avoid empty interfaces
     public interface IOutermostScope
+#pragma warning restore CA1040 // Avoid empty interfaces
     {
     }
 }
